fbd26938d6a74d8e2d45c758cdb3079f1026b2c7
johynpapin <johynpapin@kiwiasso.org>
2017-03-25 15:40:24 +0100

    42

--- client/imports/pages/register/register.ts
+++ client/imports/pages/register/register.ts
@@ -1,11 +1,21 @@
 import {Component} from '@angular/core';
 import template from './register.html';
+import {NavController} from "ionic-angular";
+import {HomePage} from "../home/home";
 
 @Component({
     template
 })
 export class RegisterPage {
-    constructor() {
+    constructor(private navCtrl: NavController) {
 
+    }
+
+    register() {
+        this.navCtrl.setRoot(HomePage).then(() => {
+            console.log('ok');
+        }).catch(e => {
+            console.log(e);
+        });
     }
 }
