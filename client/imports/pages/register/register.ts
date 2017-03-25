cc87197dfef8cdc1abf3d953858457d0d4532f15
johynpapin <johynpapin@kiwiasso.org>
2017-03-25 18:36:45 +0100

    Changed swiper color

--- client/imports/pages/register/register.ts
+++ client/imports/pages/register/register.ts
@@ -1,6 +1,6 @@
 import {Component} from '@angular/core';
 import template from './register.html';
-import {NavController} from "ionic-angular";
+import {NavController, ToastController, LoadingController} from "ionic-angular";
 import {HomePage} from "../home/home";
 import {Validators, FormBuilder, FormGroup} from '@angular/forms';
 
@@ -10,17 +10,51 @@ import {Validators, FormBuilder, FormGroup} from '@angular/forms';
 export class RegisterPage {
     private registerForm: FormGroup;
 
-    constructor(private navCtrl: NavController, private formBuilder: FormBuilder) {
+    constructor(private navCtrl: NavController, private formBuilder: FormBuilder, public loadingCtrl: LoadingController, private toastCtrl: ToastController) {
         this.registerForm = this.formBuilder.group({
             nickname: ['', Validators.required],
             lastname: ['', Validators.required],
             firstname: ['', Validators.required],
             email: ['', Validators.compose([Validators.required, Validators.email])],
-            password: ['', Validators.required]
+            password: ['', Validators.required] // TODO : vérifier que le re soit égal au mot de passe
         });
     }
 
     register() {
-        console.log(this.registerForm.value);
+        let v = this.registerForm.value;
+
+        let loading = this.loadingCtrl.create({
+            content: 'Inscription en cours...'
+        });
+
+        loading.present();
+
+        Accounts.createUser({
+            username: v.nickname,
+            email: v.email,
+            password: v.password,
+            profile: {
+                lastName: v.lastname,
+                firstName: v.firstname
+            }
+        }, (e) => {
+            loading.dismiss();
+            if (e) {
+                let toast = this.toastCtrl.create({
+                    message: 'Erreur : ' + e,
+                    duration: 3000,
+                    showCloseButton: true
+                });
+                toast.present();
+            } else {
+                let toast = this.toastCtrl.create({
+                    message: 'Vous êtes désormais inscrit, bienvenue !',
+                    duration: 3000,
+                    showCloseButton: true
+                });
+                toast.present();
+                this.navCtrl.setRoot(HomePage);
+            }
+        });
     }
 }
