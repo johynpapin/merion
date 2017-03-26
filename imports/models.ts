de6959b166b525c21475b962b277da7a6f7421c4
johynpapin <johynpapin@kiwiasso.org>
2017-03-26 01:08:28 +0100

    woaw it’s working so good

--- imports/models.ts
+++ imports/models.ts
@@ -2,3 +2,10 @@ export interface Location {
     lat: number;
     lng: number;
 }
+
+export interface Trip {
+    destination: Location;
+    matchpoint: Location;
+    enddate: Date;
+    [index: number]: string;
+}
