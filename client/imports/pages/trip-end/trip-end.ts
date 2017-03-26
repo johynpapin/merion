93273c37737fc54f39d46c153c1cbbe45fbd5c96
Quentin RIBAC <ribac.quentin@gmail.com>
2017-03-26 10:35:37 +0200

    42

--- client/imports/pages/trip-end/trip-end.ts
+++ client/imports/pages/trip-end/trip-end.ts
@@ -1,6 +1,7 @@
 import {Component} from '@angular/core';
 import template from './trip-end.html';
-import {NavParams, ViewController} from "ionic-angular";
+import {NavParams, ViewController, NavController} from "ionic-angular";
+import {HomePage} from '../home/home';
 
 @Component({
 	selector: 'trip-end-page',
@@ -10,4 +11,9 @@ export class TripEndPage {
     constructor(private params: NavParams, private viewCtrl: ViewController) {
 
     }
+
+	showHome() {
+		this.natCtrl.setRoot(HomePage);
+		this.navCtrl.popToRoot();
+	}
 }
